beeb2920d67a8f30dac5529eedbc46487b6be551
misha <1800misha@gmail.com>
2017-11-11 14:48:29 -0500

    first commit

---
+++ truffle.js
@@ -0,0 +1,16 @@
+module.exports = {
+  migrations_directory: "./migrations",
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    test: {
+      host: "localhost",
+      port: 8545,
+      from: "0x95e24955386e38fb7a52DF2647B67cB4f3D4C18d",
+      network_id: "*" // Match any network id
+    }
+  }
+};
